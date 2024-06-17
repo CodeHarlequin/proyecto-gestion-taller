d79339a955e31d6338c90ce86b0fbf35c3415671
Francisco Baena <insfrana@outlook.com>
2024-06-17 13:23:13 +0200

    Validación de la descripción la la clase Cita

--- src/BibliotecaClases/ModeloInfoGestion/Cita.cs
+++ src/BibliotecaClases/ModeloInfoGestion/Cita.cs
@@ -4,66 +4,87 @@ using Usuarios;
 
 namespace ModeloInfoGestion
 {
-   public class Cita
-   {
-      #region CONSTANTES
+    public class Cita
+    {
+        #region CONSTANTES
+        private const int DESCRIPCION_MAX = 300;
+        #endregion
 
-      #endregion
+        #region MIEMBROS
+        private DateTime _fechaMasHora;
 
-      #region MIEMBROS
-      private DateTime _fechaMasHora;
+        // Datos complentacion de la cita
+        private string _descripcion;
 
-      // Datos complentacion de la cita
-      private string _descripcion;
+        // Datos de indentificacion de los objetos implicados
+        private Cliente _solicitante;
+        private Vehiculo _vehiculoReparar;
+        #endregion
 
-      // Datos de indentificacion de los objetos implicados
-      private Cliente _solicitante;
-      private Vehiculo _vehiculoReparar;
-      #endregion
+        #region CONSTRUCTORES
+        public Cita(DateTime fecha, TimeSpan hora, string descripcion, Cliente solicitante, Vehiculo vehiculo)
+        {
+            FechaMasHora = fecha + hora;
+            Solicitante = solicitante;
+            VehiculoReparar = vehiculo;
+            Descripcion = descripcion;
+        }
 
-      #region CONSTRUCTORES
-      public Cita(DateTime fecha, TimeSpan hora, string descripcion, Cliente solicitante, Vehiculo vehiculo)
-      {
-         FechaMasHora = fecha + hora;
-         Solicitante = solicitante;
-         VehiculoReparar = vehiculo;
-         Descripcion = descripcion;
-      }
+        #endregion
 
-      #endregion
+        #region PROPIEDADES
+        public DateTime FechaMasHora
+        {
+            get => _fechaMasHora;
+            set => _fechaMasHora = value;
+        }
 
-      #region PROPIEDADES
-      public DateTime FechaMasHora
-      {
-         get => _fechaMasHora;
-         set => _fechaMasHora = value;
-      }
+        public Cliente Solicitante
+        {
+            get => _solicitante;
+            set => _solicitante = value;
+        }
 
-      public Cliente Solicitante
-      {
-         get => _solicitante;
-         set => _solicitante = value;
-      }
+        public string Descripcion
+        {
+            get => _descripcion;
+            set
+            {
+                _descripcion = ValidarDescripcion(value);
+            }
+        }
 
-      public string Descripcion
-      {
-         get => _descripcion;
-         set => _descripcion = value;
-      }
+        public Vehiculo VehiculoReparar
+        {
+            get => _vehiculoReparar;
+            set => _vehiculoReparar = value;
+        }
+        #endregion
 
-      public Vehiculo VehiculoReparar
-      {
-         get => _vehiculoReparar;
-         set => _vehiculoReparar = value;
-      }
-      #endregion
+        #region METODOS
 
-      #region METODOS
-      // TODO: Metodo de validacion de la descripcion
-      #endregion
-   }
 
-   #region EXCEPCIONES PERSONALIZADAS
+        /// <returns>Devuelve la cadena sin espacios inecesarios</returns>
+        private static string ValidarDescripcion(string cadena)
+        {
+            // Preparación de la cadena
+            cadena = cadena.Trim();
 
-   #endregion
+            // Validación del rango maximo de la descripción
+            if (cadena.Length > DESCRIPCION_MAX) throw new DescripcionErroneaException($"La descripción es mayor al rango máximo, (0 - {DESCRIPCION_MAX})");
+
+            return cadena;
+        }
+
+        #endregion
+    }
+
+    #region EXCEPCIONES PERSONALIZADAS
+    public class DescripcionErroneaException : Exception
+    {
+        public DescripcionErroneaException() : base("El formato de la descripción es incorrecta.") { }
+        public DescripcionErroneaException(string message) : base(message) { }
+    }
+
+    #endregion
 }
