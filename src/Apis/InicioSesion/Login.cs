98e6752d8f256c2f60a8d993c40bea63985f211c
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-06-17 18:22:11 +0200

    Implemantacion de la api encarga de la verificacion de si un usuario se encuntra registrado en la base de datos

---
+++ src/Apis/InicioSesion/Login.cs
@@ -0,0 +1,63 @@
+﻿using BaseDatos;
+using Usuarios;
+
+namespace InicioSesion
+{
+   public static class ApiLoguin
+   {
+      public static void Login<TClaPersona>(object usuarioVerificar) where TClaPersona : Persona
+      {
+         // Recursos locales
+         Persona usuarioLoguear = (TClaPersona)usuarioVerificar;
+         string consultaRealizar;
+
+         try
+         {
+            // Construye la consulta a realizar 
+            consultaRealizar = ConstruirConsulta(usuarioLoguear);
+
+            // Ejecutar la consulta sobre la base de datos 
+            usuarioLoguear = (TClaPersona)ApiBaseDatos.OptenerListaObjetos(consultaRealizar)[0];
+
+            // Comprobara la contraseña del usaurio obtenido
+            if (usuarioLoguear.Contrasenia != usuarioLoguear.Contrasenia)
+               throw new ContraseniaIncorrectaException();
+
+         }
+         // En caso de devolver un array vacio no esposible acceder a la posicion 0 significara que el usuario no existe
+         catch (Exception error) // La deteccion de otro error 
+         {
+            throw new Exception($"Login:{error.Message}");
+         }
+      }
+
+      private static string ConstruirConsulta(Persona usuarioVerificar)
+      {
+         // Recursos locales
+         string nombreTabla = "";
+         string consultaDevolver = "";
+
+         // Evaluacion del tipo de clase (Heredan de Persona)
+         switch (usuarioVerificar)
+         {
+            case Cliente:
+               nombreTabla = "Clientes ";
+               break;
+            case Empleado:
+               nombreTabla = "Mecanicos";
+               break;
+         }
+
+         // Se formatea la consulta a realizar
+         consultaDevolver = $"SELECT * FROM {nombreTabla} " +
+            $"WHERE cDni = '{usuarioVerificar.Dni}' AND vContrasenia = '{usuarioVerificar.Contrasenia}'";
+
+         return consultaDevolver;
+      }
+   }
+
+   public class ContraseniaIncorrectaException : Exception
+   {
+      public ContraseniaIncorrectaException() : base("La contrasela es incorrecta") { }
+   }
+}
