ad798a141d5c8faeca286b76d772b97f392dd350
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-06-19 11:00:22 +0200

    Modificacion de la clase propietario para ser independiente de persona

--- src/BibliotecaClases/ModeloUsuarios/Propietario.cs
+++ src/BibliotecaClases/ModeloUsuarios/Propietario.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Usuarios
 {
-   public class Propietario : Persona
+   public class Propietario
    {
-      public Propietario(string usuario, string contrasenia) : base(usuario, contrasenia) { }
+      private string _usuario;
+      private string _contrasenia;
+
+      public Propietario(string usuario, string contrasenia) 
+      { 
+         Usuario = usuario;
+         Contrasenia = contrasenia;
+      }
+
+      public string Usuario 
+      { 
+         get => _usuario; 
+         set => _usuario = value; 
+      }
+
+      public string Contrasenia
+      {
+         get => _contrasenia;
+         set => _contrasenia = value;
+      }
    }
 }
