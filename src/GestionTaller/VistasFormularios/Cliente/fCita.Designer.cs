17711607019d86657487b2c26916b343d7a8b06d
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-06-19 10:15:59 +0200

    Creación de los fromularios correspondientes al Cliente

---
+++ src/GestionTaller/VistasFormularios/Cliente/fCita.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GestionTaller
+{
+   partial class fCita
+   {
+      /// <summary>
+      /// Variable del diseñador necesaria.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Limpiar los recursos que se estén usando.
+      /// </summary>
+      /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Código generado por el Diseñador de Windows Forms
+
+      /// <summary>
+      /// Método necesario para admitir el Diseñador. No se puede modificar
+      /// el contenido de este método con el editor de código.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         components = new System.ComponentModel.Container();
+         infoError = new ErrorProvider(components);
+         lVehiculos = new Label();
+         lbVehiculos = new ListBox();
+         lFechaHora = new Label();
+         lDescipcion = new Label();
+         tbDescripcion = new TextBox();
+         dtpFecha = new DateTimePicker();
+         bGuardar = new Button();
+         mtbHora = new MaskedTextBox();
+         ((System.ComponentModel.ISupportInitialize)infoError).BeginInit();
+         SuspendLayout();
+         // 
+         // infoError
+         // 
+         infoError.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+         infoError.ContainerControl = this;
+         // 
+         // lVehiculos
+         // 
+         lVehiculos.AutoSize = true;
+         lVehiculos.Location = new Point(26, 20);
+         lVehiculos.Margin = new Padding(4, 0, 4, 0);
+         lVehiculos.Name = "lVehiculos";
+         lVehiculos.Size = new Size(103, 15);
+         lVehiculos.TabIndex = 0;
+         lVehiculos.Text = "Lista de Vehículos:";
+         // 
+         // lbVehiculos
+         // 
+         lbVehiculos.FormattingEnabled = true;
+         lbVehiculos.HorizontalScrollbar = true;
+         lbVehiculos.ItemHeight = 15;
+         lbVehiculos.Location = new Point(29, 38);
+         lbVehiculos.Margin = new Padding(4, 3, 4, 3);
+         lbVehiculos.Name = "lbVehiculos";
+         lbVehiculos.ScrollAlwaysVisible = true;
+         lbVehiculos.Size = new Size(613, 109);
+         lbVehiculos.TabIndex = 1;
+         // 
+         // lFechaHora
+         // 
+         lFechaHora.AutoSize = true;
+         lFechaHora.Location = new Point(26, 172);
+         lFechaHora.Margin = new Padding(4, 0, 4, 0);
+         lFechaHora.Name = "lFechaHora";
+         lFechaHora.Size = new Size(79, 15);
+         lFechaHora.TabIndex = 2;
+         lFechaHora.Text = "Fecha y Hora:";
+         // 
+         // lDescipcion
+         // 
+         lDescipcion.AutoSize = true;
+         lDescipcion.Location = new Point(26, 218);
+         lDescipcion.Margin = new Padding(4, 0, 4, 0);
+         lDescipcion.Name = "lDescipcion";
+         lDescipcion.Size = new Size(72, 15);
+         lDescipcion.TabIndex = 4;
+         lDescipcion.Text = "Descripción:";
+         // 
+         // tbDescripcion
+         // 
+         tbDescripcion.Location = new Point(29, 237);
+         tbDescripcion.Margin = new Padding(4, 3, 4, 3);
+         tbDescripcion.MaxLength = 300;
+         tbDescripcion.Multiline = true;
+         tbDescripcion.Name = "tbDescripcion";
+         tbDescripcion.Size = new Size(613, 101);
+         tbDescripcion.TabIndex = 5;
+         // 
+         // dtpFecha
+         // 
+         dtpFecha.CustomFormat = "dd/MM/yyyy";
+         dtpFecha.Format = DateTimePickerFormat.Custom;
+         dtpFecha.Location = new Point(119, 167);
+         dtpFecha.Margin = new Padding(4, 3, 4, 3);
+         dtpFecha.Name = "dtpFecha";
+         dtpFecha.Size = new Size(114, 23);
+         dtpFecha.TabIndex = 3;
+         dtpFecha.Value = new DateTime(2024, 6, 18, 0, 0, 0, 0);
+         // 
+         // bGuardar
+         // 
+         bGuardar.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point);
+         bGuardar.Location = new Point(442, 358);
+         bGuardar.Margin = new Padding(4, 3, 4, 3);
+         bGuardar.Name = "bGuardar";
+         bGuardar.Size = new Size(201, 37);
+         bGuardar.TabIndex = 9;
+         bGuardar.Text = "Guardar";
+         bGuardar.UseVisualStyleBackColor = true;
+         // 
+         // mtbHora
+         // 
+         mtbHora.Location = new Point(239, 166);
+         mtbHora.Mask = "00:00";
+         mtbHora.Name = "mtbHora";
+         mtbHora.Size = new Size(30, 23);
+         mtbHora.TabIndex = 10;
+         mtbHora.ValidatingType = typeof(DateTime);
+         // 
+         // fCita
+         // 
+         AutoScaleDimensions = new SizeF(7F, 15F);
+         AutoScaleMode = AutoScaleMode.Font;
+         ClientSize = new Size(672, 420);
+         Controls.Add(mtbHora);
+         Controls.Add(bGuardar);
+         Controls.Add(dtpFecha);
+         Controls.Add(tbDescripcion);
+         Controls.Add(lDescipcion);
+         Controls.Add(lFechaHora);
+         Controls.Add(lbVehiculos);
+         Controls.Add(lVehiculos);
+         Margin = new Padding(4, 3, 4, 3);
+         Name = "fCita";
+         Text = "Taller: Pedir Cita";
+         ((System.ComponentModel.ISupportInitialize)infoError).EndInit();
+         ResumeLayout(false);
+         PerformLayout();
+      }
+
+      #endregion
+      private ErrorProvider infoError;
+      private TextBox tbDescripcion;
+      private Label lDescipcion;
+      private Label lFechaHora;
+      private ListBox lbVehiculos;
+      private Label lVehiculos;
+      private DateTimePicker dtpFecha;
+      private Button bGuardar;
+      private MaskedTextBox mtbHora;
+   }
+}
+
---
+++ src/GestionTaller/VistasFormularios/Cliente/fVehiculo.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace GestionTaller
+{
+    partial class fVehiculo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.infoError = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tbContrasenia = new System.Windows.Forms.TextBox();
+            this.lMatricula = new System.Windows.Forms.Label();
+            this.tbNBastidor = new System.Windows.Forms.TextBox();
+            this.lNBastidor = new System.Windows.Forms.Label();
+            this.lVehiculo = new System.Windows.Forms.Label();
+            this.tbMarca = new System.Windows.Forms.TextBox();
+            this.lMarca = new System.Windows.Forms.Label();
+            this.tbModelo = new System.Windows.Forms.TextBox();
+            this.lModelo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbTipoVehiculo = new System.Windows.Forms.ComboBox();
+            this.bGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.infoError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // infoError
+            // 
+            this.infoError.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.infoError.ContainerControl = this;
+            // 
+            // tbContrasenia
+            // 
+            this.tbContrasenia.Location = new System.Drawing.Point(142, 109);
+            this.tbContrasenia.MaxLength = 8;
+            this.tbContrasenia.Name = "tbContrasenia";
+            this.tbContrasenia.Size = new System.Drawing.Size(181, 20);
+            this.tbContrasenia.TabIndex = 4;
+            // 
+            // lMatricula
+            // 
+            this.lMatricula.AutoSize = true;
+            this.lMatricula.Location = new System.Drawing.Point(81, 112);
+            this.lMatricula.Name = "lMatricula";
+            this.lMatricula.Size = new System.Drawing.Size(55, 13);
+            this.lMatricula.TabIndex = 3;
+            this.lMatricula.Text = "Matrícula:";
+            // 
+            // tbNBastidor
+            // 
+            this.tbNBastidor.Location = new System.Drawing.Point(142, 69);
+            this.tbNBastidor.MaxLength = 17;
+            this.tbNBastidor.Name = "tbNBastidor";
+            this.tbNBastidor.Size = new System.Drawing.Size(181, 20);
+            this.tbNBastidor.TabIndex = 2;
+            // 
+            // lNBastidor
+            // 
+            this.lNBastidor.AutoSize = true;
+            this.lNBastidor.Location = new System.Drawing.Point(35, 72);
+            this.lNBastidor.Name = "lNBastidor";
+            this.lNBastidor.Size = new System.Drawing.Size(102, 13);
+            this.lNBastidor.TabIndex = 1;
+            this.lNBastidor.Text = "Número de bastidor:";
+            // 
+            // lVehiculo
+            // 
+            this.lVehiculo.AutoSize = true;
+            this.lVehiculo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lVehiculo.Location = new System.Drawing.Point(29, 21);
+            this.lVehiculo.Name = "lVehiculo";
+            this.lVehiculo.Size = new System.Drawing.Size(294, 22);
+            this.lVehiculo.TabIndex = 0;
+            this.lVehiculo.Text = "Introduzca los datos de su vehículo:";
+            // 
+            // tbMarca
+            // 
+            this.tbMarca.Location = new System.Drawing.Point(142, 147);
+            this.tbMarca.MaxLength = 50;
+            this.tbMarca.Name = "tbMarca";
+            this.tbMarca.Size = new System.Drawing.Size(181, 20);
+            this.tbMarca.TabIndex = 6;
+            // 
+            // lMarca
+            // 
+            this.lMarca.AutoSize = true;
+            this.lMarca.Location = new System.Drawing.Point(96, 150);
+            this.lMarca.Name = "lMarca";
+            this.lMarca.Size = new System.Drawing.Size(40, 13);
+            this.lMarca.TabIndex = 5;
+            this.lMarca.Text = "Marca:";
+            // 
+            // tbModelo
+            // 
+            this.tbModelo.Location = new System.Drawing.Point(142, 184);
+            this.tbModelo.MaxLength = 50;
+            this.tbModelo.Name = "tbModelo";
+            this.tbModelo.Size = new System.Drawing.Size(181, 20);
+            this.tbModelo.TabIndex = 8;
+            // 
+            // lModelo
+            // 
+            this.lModelo.AutoSize = true;
+            this.lModelo.Location = new System.Drawing.Point(91, 187);
+            this.lModelo.Name = "lModelo";
+            this.lModelo.Size = new System.Drawing.Size(45, 13);
+            this.lModelo.TabIndex = 7;
+            this.lModelo.Text = "Modelo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tipo de vehículo:";
+            // 
+            // cbTipoVehiculo
+            // 
+            this.cbTipoVehiculo.FormattingEnabled = true;
+            this.cbTipoVehiculo.Location = new System.Drawing.Point(142, 224);
+            this.cbTipoVehiculo.Name = "cbTipoVehiculo";
+            this.cbTipoVehiculo.Size = new System.Drawing.Size(181, 21);
+            this.cbTipoVehiculo.TabIndex = 10;
+            // 
+            // bGuardar
+            // 
+            this.bGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bGuardar.Location = new System.Drawing.Point(33, 274);
+            this.bGuardar.Name = "bGuardar";
+            this.bGuardar.Size = new System.Drawing.Size(290, 32);
+            this.bGuardar.TabIndex = 11;
+            this.bGuardar.Text = "Guardar";
+            this.bGuardar.UseVisualStyleBackColor = true;
+            // 
+            // fVehiculo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 329);
+            this.Controls.Add(this.bGuardar);
+            this.Controls.Add(this.cbTipoVehiculo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbModelo);
+            this.Controls.Add(this.lModelo);
+            this.Controls.Add(this.tbMarca);
+            this.Controls.Add(this.lMarca);
+            this.Controls.Add(this.lVehiculo);
+            this.Controls.Add(this.tbContrasenia);
+            this.Controls.Add(this.lMatricula);
+            this.Controls.Add(this.tbNBastidor);
+            this.Controls.Add(this.lNBastidor);
+            this.Name = "fVehiculo";
+            this.Text = "Taller: Registrar Vehículo";
+            ((System.ComponentModel.ISupportInitialize)(this.infoError)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ErrorProvider infoError;
+        private System.Windows.Forms.TextBox tbContrasenia;
+        private System.Windows.Forms.Label lMatricula;
+        private System.Windows.Forms.TextBox tbNBastidor;
+        private System.Windows.Forms.Label lNBastidor;
+        private System.Windows.Forms.Label lVehiculo;
+        private System.Windows.Forms.TextBox tbModelo;
+        private System.Windows.Forms.Label lModelo;
+        private System.Windows.Forms.TextBox tbMarca;
+        private System.Windows.Forms.Label lMarca;
+        private System.Windows.Forms.ComboBox cbTipoVehiculo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bGuardar;
+    }
+}
+
