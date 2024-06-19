37ae3a08f8eda1f9176fbb3efe57c20fbd4c873f
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-06-19 10:59:38 +0200

    Optenecia de los datos relacinados con el propietario

---
+++ src/Apis/GestConfTaller/DatosPropietario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Usuarios;
+
+namespace GestConfTaller
+{
+   public class DatosPropietario
+   { 
+      public const string NOMBRE = "PropieatarioTaller";
+      public const string EXTENCION = ".p";
+      
+      public static Propietario OptenePropietario()
+      {
+         string registroDatos;
+         Propietario propietarioDevolver = null;
+
+         // Optiene los registros del fichero
+         registroDatos = File.ReadAllLines($"..\\..\\..\\..\\src\\Configuracion\\{NOMBRE}{EXTENCION}")[0];
+
+         if (registroDatos != null)
+         {
+            propietarioDevolver = new Propietario(
+               registroDatos.Split(";")[0], 
+               registroDatos.Split(";")[1]);
+         }
+
+
+         return propietarioDevolver;
+      }
+   }
+}
