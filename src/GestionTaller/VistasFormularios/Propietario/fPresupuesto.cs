ad0bb9b6394fc3f0f6bba245f84f61177859f111
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-06-19 10:17:15 +0200

    Creación de los fromularios correspondientes al Propietario

---
+++ src/GestionTaller/VistasFormularios/Propietario/fPresupuesto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionTaller.VistasFormularios.Propietario
+{
+   public partial class fPresupuesto : Form
+   {
+      public fPresupuesto()
+      {
+         InitializeComponent();
+      }
+   }
+}
