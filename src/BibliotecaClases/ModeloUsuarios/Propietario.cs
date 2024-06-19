55d23d416b75b06735becc9ec9ced98aec8d1e7f
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-06-19 10:59:14 +0200

    Creacion de una nueva clase de usuario

---
+++ src/BibliotecaClases/ModeloUsuarios/Propietario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuarios
+{
+   public class Propietario : Persona
+   {
+      public Propietario(string usuario, string contrasenia) : base(usuario, contrasenia) { }
+   }
+}
