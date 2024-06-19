1116d6e57c557ce1fc9b742f81d08a49db918e91
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-06-19 16:43:56 +0200

    Agregacion de funcionalidad para la vista del propietariode para la ver la lista de reparaciones

---
+++ src/GestionTaller/VistasFormularios/Propietario/LogicaNegocio.cs
@@ -0,0 +1,52 @@
+﻿using BaseDatos;
+using InfoGestion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Usuarios;
+
+namespace GestionTaller.VistasFormularios.Propietario
+{
+   public static class LogicaNegocio
+   {
+      public static TObj[] OptenerListaObjetos<TObj>()
+      {
+         object[] listaObtenida;
+         TObj[] listaDevolver = null;
+         string consultaRealizar = "";
+
+         // Inializacion 
+         switch (typeof(TObj).Name)
+         {
+            case nameof(Reparacion): // iCodigo_Reparacion, cNBastidor, dFecha_Inicio, dFecha_Fin, vEstado, fPresupuesto
+               consultaRealizar = "SELECT * FROM Reparaciones";
+               break;
+            case nameof(Empleado):
+               consultaRealizar = "SELECT * FROM Mecanicos";
+               break;
+         }
+
+         // Proceso de optencion de los valores
+         listaObtenida = ApiBaseDatos.OptenerListaObjetos(consultaRealizar);
+
+         // Conversion del cada elemento de la lista al tipo adecuado
+         if (listaObtenida != null)
+            listaDevolver = listaObtenida.Select(obj => (TObj)obj).ToArray();
+
+         // Devolucion de la lista
+         return listaDevolver;
+      }
+
+      public static Reparacion OptenerDatosReparacion(int codigoReparacion)
+      {
+         return (Reparacion)ApiBaseDatos.OptenerListaObjetos($"SELECT * FROM Reparaciones WHERE iCodigo_Reparacion = {codigoReparacion}")[0];
+      }
+
+      public static void ActualizarPresupuesto(Reparacion reparacionModificar)
+      {
+         ApiBaseDatos.EjecutarInstruccion($"UPDATE Reparaciones SET fPresupuesto = {reparacionModificar.Presupuesto} WHERE iCodigo_Reparacion = '{reparacionModificar.CodigoRep}'");
+      }
+   }
+}
--- src/GestionTaller/VistasFormularios/Propietario/fReparaciones.cs
+++ src/GestionTaller/VistasFormularios/Propietario/fReparaciones.cs
@@ -1,4 +1,5 @@
 ﻿using GestionTaller.VistasFormularios.Propietario;
+using InfoGestion;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,6 +17,95 @@ namespace GestionTaller
       public fReparaciones()
       {
          InitializeComponent();
+      }
+
+      private void fReparaciones_Load(object sender, EventArgs e)
+      {
+         // Se desabilitan los controles 
+         DesabilitarBotonesAccion();
+
+         // Rellena el data grid 
+         ActualizarDataGridView();
+
+         // Ocultar columnas inecesarios
+         OcultarColumnasInecesarias();
+      }
+
+      private void dgvReparaciones_CellClick(object sender, DataGridViewCellEventArgs e)
+      {
+         // Recursos locales
+         string valorCelda;
+
+         // Inializaccion
+         valorCelda = dgvReparaciones.SelectedRows[0].Cells["EstadoRep"].Value.ToString();
+         DesabilitarBotonesAccion();
+
+         // Comprobaciones
+         // Depenidndo de la fila selecciona se habilita unos controles u otros
+         if (valorCelda == Reparacion.Estado.Diagnostico.ToString())
+            bPresupuesto.Enabled = true;
+
+         if (valorCelda == Reparacion.Estado.Aprobacion_Cliente.ToString())
+            bTrabajadores.Enabled = true;
+      }
+
+      private void bAcccion_Click(object sender, EventArgs e)
+      {
+         // Recursos locales
+         Form formularioMostrar = null;
+         Button botonClikado;
+         Reparacion reparacionSeleccionada;
+
+         // Inializacion
+         botonClikado = (Button)sender;
+         reparacionSeleccionada = LogicaNegocio.OptenerDatosReparacion(
+            (int)dgvReparaciones.SelectedRows[0].Cells["CodigoRep"].Value);
+
+         switch (botonClikado.Name)
+         {
+            case "bPresupuesto":
+               formularioMostrar = new fPresupuesto();
+               break;
+            case "bTrabajadores":
+               formularioMostrar = new fSelectTrabajadores();
+               break;
+         }
+
+         switch (formularioMostrar)
+         {
+            case fPresupuesto formReparacion:
+               formReparacion.RepModificar = reparacionSeleccionada;
+               break;
+               //case fSelectTrabajadores formMecanico:
+               //   formMecanico.RepModificar = reparacionSeleccionada;
+               //   break;
+         }
+
+         // Mostrar formulario
+         formularioMostrar.ShowDialog();
+
+         // Actualizar los datos del Datagrid
+         if (formularioMostrar.DialogResult == DialogResult.OK)
+            ActualizarDataGridView();
+      }
+
+      private void ActualizarDataGridView()
+      {
+         //dgvReparaciones.DataSource = LogicaNegocio.OptenerListaReparacaiones().ToList();
+         dgvReparaciones.DataSource = LogicaNegocio.OptenerListaObjetos<Reparacion>().ToList();
+      }
+
+      private void OcultarColumnasInecesarias()
+      {
+         dgvReparaciones.Columns["VehiculoImplicado"].Visible = false;
+         dgvReparaciones.Columns["JefeMecanico"].Visible = false;
+         dgvReparaciones.Columns["InformacionAdicional"].Visible = false;
+      }
+
+      private void DesabilitarBotonesAccion()
+      {
+         bPresupuesto.Enabled = false;
+         bTrabajadores.Enabled = false;
       }
    }
 }
