77884e032535f41f3d3d34ee8633077617ba126b
María Carvajal Blanco <mariacbdam@outlook.com>
2024-06-21 08:39:32 +0200

    Finalización de los Formularios del Empleado

---
+++ src/GestionTaller/VistasFormularios/Mecanico/LogicaNegocio.cs
@@ -0,0 +1,77 @@
+﻿using BaseDatos;
+using InfoGestion;
+using ModeloVehiculos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTaller.VistasFormularios.Mecanico
+{
+    public static class LogicaNegocio
+    {
+        public static Reparacion[] ObtenerListaReparaciones(string codEmpleado)
+        {
+            object[] listaObtenida;
+            Reparacion[] listaDevolver = null;
+            string consultaRealizar;
+
+            // Inializacion 
+            consultaRealizar = $"SELECT * FROM Reparaciones WHERE iCodigo_Reparacion IN (SELECT iCodigo_Reparacion FROM MecanicoReparacion WHERE cDni_Mecanico = '{codEmpleado}') AND vEstado = 'Reparacion'";
+
+            // Proceso de optencion de los valores
+            listaObtenida = ApiBaseDatos.OptenerListaObjetos(consultaRealizar);
+
+            // Conversion del cada elemento de la lista al tipo adecuado
+            if (listaObtenida != null)
+                listaDevolver = listaObtenida.Select(obj => (Reparacion)obj).ToArray();
+
+            // Devolucion de la lista
+            return listaDevolver;
+        }
+
+        public static Reparacion ObtenerDatosReparacion(int codigoReparacion)
+        {
+            return (Reparacion)ApiBaseDatos.OptenerListaObjetos($"SELECT * FROM Reparaciones WHERE iCodigo_Reparacion = {codigoReparacion}")[0];
+        }
+
+        public static Vehiculo ObtenerDatosVehiculo(string numBastidor)
+        {
+            return (Vehiculo)ApiBaseDatos.OptenerListaObjetos($"SELECT * FROM Vehiculos WHERE cNBastidor = '{numBastidor}'")[0];
+        }
+
+        public static Operacion[] ObtenerListaOperaciones()
+        {
+            object[] listaObtenida;
+            Operacion[] listaDevolver = null;
+            string consultaRealizar;
+
+            // Inializacion 
+            consultaRealizar = $"SELECT * FROM Operaciones";
+
+            // Proceso de optencion de los valores
+            listaObtenida = ApiBaseDatos.OptenerListaObjetos(consultaRealizar);
+
+            // Conversion del cada elemento de la lista al tipo adecuado
+            if (listaObtenida != null)
+                listaDevolver = listaObtenida.Select(obj => (Operacion)obj).ToArray();
+
+            // Devolucion de la lista
+            return listaDevolver;
+        }
+
+        internal static void AlmacenarOperacionesRealizadas(int codigoRep, Operacion[] listaOperaciones)
+        {
+            for (int indice = 0; indice < listaOperaciones.Length; indice++)
+            {
+                ApiBaseDatos.EjecutarInstruccion($"INSERT INTO OperacionReparacion(iCodigo_Reparacion, vNombre_Operacion) VALUES ({codigoRep}, '{listaOperaciones[indice].Nombre}')");
+            }
+        }
+
+        internal static void ActualizarEstado(int codigoRep)
+        {
+            ApiBaseDatos.EjecutarInstruccion($"UPDATE Reparaciones SET vEstado = 'Completado' WHERE iCodigo_Reparacion = {codigoRep}");
+        }
+    }
+}
--- src/GestionTaller/VistasFormularios/Mecanico/fConsultarReparaciones.cs
+++ src/GestionTaller/VistasFormularios/Mecanico/fConsultarReparaciones.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InfoGestion;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Usuarios;
 
 namespace GestionTaller.VistasFormularios.Mecanico
 {
-   public partial class fConsultarReparaciones : Form
-   {
-      public fConsultarReparaciones()
-      {
-         InitializeComponent();
-      }
-   }
+    public partial class fConsultarReparaciones : Form
+    {
+        private Empleado _mecanico;
+
+        public Empleado MecanicoSeleccionado
+        {
+            get => _mecanico;
+            set => _mecanico = value;
+        }
+
+        public fConsultarReparaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void fConsultarReparaciones_Load(object sender, EventArgs e)
+        {
+            // Desactivamos el botón de modificar
+            bModificar.Enabled = false;
+            bCompletarReparacion.Enabled = false;
+
+            // Cargamos la lista de consultas
+            ActualizarDataGridView();
+
+            // Ocultamos las columnas que no sean relevantes para el mecánico
+            OcultarColumnasInecesarias();
+        }
+
+        private void dgvReparaciones_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Empleado empleadoJefe;
+
+            // Activar el boton de modificar
+            bModificar.Enabled = true;
+
+            // Habitar boton si es jefe
+            empleadoJefe = (Empleado)dgvReparaciones.SelectedRows[0].Cells["JefeMecanico"].Value;
+            bCompletarReparacion.Enabled = MecanicoSeleccionado.Dni == empleadoJefe.Dni;
+        }
+
+        private void bModificar_Click(object sender, EventArgs e)
+        {
+            // Recursos Locales
+            fModificarReparacion formularioMostrar;
+            Button botonModificar;
+            Reparacion reparacionSelecionada;
+
+            // Inicialización
+            botonModificar = (Button)sender;
+            reparacionSelecionada = LogicaNegocio.ObtenerDatosReparacion((int)dgvReparaciones.SelectedRows[0].Cells["CodigoRep"].Value);
+
+            // Crear una nueva instancia del formulario de modificación
+            formularioMostrar = new fModificarReparacion();
+
+            // Pasar los datos de la reparacion seleccionada al formulario de modificación
+            formularioMostrar.ReparacionSeleccionada = reparacionSelecionada;
+
+            // Mostrar formulario para modificar la reparación
+            formularioMostrar.ShowDialog();
+
+            // Actualizar los datos del Datagrid al cerrar el formulario
+            if (formularioMostrar.DialogResult == DialogResult.OK)
+                ActualizarDataGridView();
+        }
+
+        private void ActualizarDataGridView()
+        {
+            // Obtenemos la lista de reparaciones de la base de datos
+            dgvReparaciones.DataSource = LogicaNegocio.ObtenerListaReparaciones(MecanicoSeleccionado.Dni).ToList();
+        }
+
+        private void OcultarColumnasInecesarias()
+        {
+            dgvReparaciones.Columns["VehiculoImplicado"].Visible = false;
+            dgvReparaciones.Columns["JefeMecanico"].Visible = false;
+            dgvReparaciones.Columns["InformacionAdicional"].Visible = false;
+        }
+
+        private void bCompletarReparacion_Click(object sender, EventArgs e)
+        {
+            Reparacion reparacionSelecionada;
+
+            // Inicialización
+            reparacionSelecionada = LogicaNegocio.ObtenerDatosReparacion((int)dgvReparaciones.SelectedRows[0].Cells["CodigoRep"].Value);
+
+            // Actualizar el estado de la reparación a completo
+            LogicaNegocio.ActualizarEstado(reparacionSelecionada.CodigoRep);
+
+            // Desactivar el botón 
+            bCompletarReparacion.Enabled = false;
+
+            // Actualizar los datos del DataGridView
+            ActualizarDataGridView();
+        } 
+    }
 }
